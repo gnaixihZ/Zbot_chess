ddca7befad47124907498db0bf1838df8f90eefa
gnaixihZ <97085493+gnaixihZ@users.noreply.github.com>
2022-07-19 12:14:31 +0800

    Update main.py

--- chess/main.py
+++ chess/main.py
@@ -1,5 +1,5 @@
-import pygame
-import geometry as g
+import pygame as p
+import engine as e
 
 FPS = 60
 SIDELEN = 512
@@ -7,81 +7,69 @@ sl = int(SIDELEN/8)
 WHITE = (255,255,255)
 LIGHTGRAY = (211,211,211)
 
-pygame.init()
-screen = pygame.display.set_mode((SIDELEN,SIDELEN))
-clock = pygame.time.Clock()
-font_name = pygame.font.match_font("arial")
-gs = g.GameState()
-move = g.move()
+p.init()
+screen = p.display.set_mode((SIDELEN,SIDELEN))
+clock = p.time.Clock()
+font_name = p.font.match_font("arial")
+gs = e.GameState()
+move = e.move()
 IMAGE = {}
 pieces = ['wK','wQ','wR','wB','wN','wp','bK','bQ','bR','bB','bN','bp']
 for piece in pieces:
-    IMAGE[piece] = pygame.transform.scale(pygame.image.load("images/" + piece + ".png"),(sl,sl))
+    IMAGE[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"),(sl,sl))
 
 def draw_piece():
     for w in range(8):
         for h in range(8):
             if gs.board[h][w] != "--":
-                screen.blit(IMAGE[gs.board[h][w]],pygame.Rect(sl*w,sl*h,sl,sl))
+                screen.blit(IMAGE[gs.board[h][w]],p.Rect(sl*w,sl*h,sl,sl))
 
 def main():
     running = True
     select = ()
-    clicks = []
     choosing = False
     while running:
         clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in p.event.get():
+            if event.type == p.QUIT:
                 running = False  
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                    selecting = pygame.mouse.get_pos()
+            if event.type == p.MOUSEBUTTONDOWN:
+                    selecting = p.mouse.get_pos()
                     mr = selecting[1]//sl
                     mc = selecting[0]//sl
-                    if select == (mr,mc):
-                        select = ()
-                        clicks = []
-                        move.allowed_moves = []
-                        choosing = False
-                    else:
-                        choosing = True
+                    if not choosing and ((gs.whitetomove and gs.board[mr][mc][0] == "w") or 
+                    ((not gs.whitetomove) and gs.board[mr][mc][0] == "b")):
                         select = (mr,mc)
-                        clicks.append(select)
-                        move.movable(gs.board,mr,mc)
-                        if move.allowed_moves == []:
-                            select = ()
-                            clicks = []
-                            choosing = False
-                    if len(clicks) == 1:
-                        if gs.board[clicks[0][0]][clicks[0][1]] == "--" or (gs.whitetomove and gs.board[clicks[0][0]][clicks[0][1]][0] == "b") or ((not gs.whitetomove) and gs.board[clicks[0][0]][clicks[0][1]][0] == "w"):
-                            select = ()
-                            clicks = []
-                            move.allowed_moves = []
-                            choosing = False
-                    if len(clicks) == 2:
-                        if clicks[1] in move.allowed_moves:
-                            gs.board[clicks[1][0]][clicks[1][1]] = gs.board[clicks[0][0]][clicks[0][1]]
-                            gs.board[clicks[0][0]][clicks[0][1]] = "--"
-                            for i in range(8):
-                                print(gs.board[i])
-                            select = ()
-                            clicks = []
+                        move.sellect_allowed_moves(gs.board,mr,mc)
+                        choosing = True
+                    elif choosing:
+                        if (mr,mc) in move.allowed_moves:
+                            gs.board[mr][mc] = gs.board[select[0]][select[1]]
+                            gs.board[select[0]][select[1]] = "--"
+                            move.record_move(gs.movelog1,select[0],select[1],mr,mc)
                             choosing = False
                             gs.whitetomove = not gs.whitetomove
+                            select = ()
                             move.allowed_moves = []
+                            for i in range(8):
+                                print(gs.board[i])
+                            print("")
                         else:
-                            clicks = []
-                            move.allowed_moves = []
-                            choosing = True
-                            select = (mr,mc)
-                            clicks.append(select)
-                            move.movable(gs.board,mr,mc)
-        g.draw_board(screen,SIDELEN,WHITE,LIGHTGRAY,font_name)
+                            if gs.board[mr][mc][0] == gs.board[select[0]][select[1]][0]:
+                                move.allowed_moves = []
+                                select = (mr,mc)
+                                move.sellect_allowed_moves(gs.board,mr,mc)
+                            else:
+                                choosing = False
+                                select = ()
+                                move.allowed_moves = []
+
+        e.draw_board(screen,SIDELEN,WHITE,LIGHTGRAY,font_name)
         draw_piece()
         if choosing:
-            g.move.display_move(move,screen,gs.board)
-        pygame.display.update()
-    pygame.quit()
+            e.move.display_allowed_move(move,screen,gs.board)
+        p.display.update()
+    p.quit()
 
 if __name__ == "__main__":
     main()
