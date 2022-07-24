47a743a8c3c01a7e97da17b9cd12e758cc32c39c
gnaixihZ <97085493+gnaixihZ@users.noreply.github.com>
2022-07-24 23:56:59 +0800

    Update engine.py

--- chess/engine.py
+++ chess/engine.py
@@ -14,140 +14,238 @@ class GameState:
         self.whitetomove = True
         self.movelog1 = []
         self.movelog2 = []
-        self.check = False
+        self.check = [False,False]
         self.checkmate = False
 
 class move:
     def __init__(self):
         self.allowed_moves = []
+        self.threated_squares = [[],[]]
         self.special_moves = [[],[],[]] #Castling,En passant,Promotion
         self.color = {0:"w",1:"b"}
         self.castling = [[True,True],[True,True]]
         self.rowdic = {0:"8",1:"7",2:"6",3:"5",4:"4",5:"3",6:"2",7:"1"}
         self.coldic = {0:"a",1:"b",2:"c",3:"d",4:"e",5:"f",6:"g",7:"h"}
+        self.wk = (7,4)
+        self.bk = (0,4)
 
-    def sellect_allowed_moves(self,board,movelog,sr,sc):
+    def find_allowed_moves(self,board,movelog,sr,sc):
         if board[sr][sc] == "--":
             return 1
+        elif board[sr][sc][0] == "w":
+            color = 0
+        else:
+            color = 1
         if board[sr][sc][1] == "p":
-            for color in range(2):
-                if board[sr][sc][0] == self.color[color]:
-                    if sr == 6 - color * 5 and board[5 - color * 3][sc] == "--" and board[4 - color][sc] == "--":
-                        self.allowed_moves.append((5 - color * 3,sc))
-                        self.allowed_moves.append((4 - color,sc))
-                    elif sr - 1 + color * 2 in range(8) and board[sr - 1 + color * 2][sc] == "--":
-                        self.allowed_moves.append((sr - 1 + color * 2,sc))
-                        if sr == 1 + color * 5:
-                            self.special_moves[2].append((sr - 1 + color * 2,sc))
-                    for i in [-1,1]:
-                        if sc + i in range(8) and (board[sr - 1 + color * 2][sc + i][0] == self.color[(color + 1) % 2]):
-                            self.allowed_moves.append((sr - 1 + color * 2,sc + i))
-                        if sc + i in range(8) and (sr - 2 + color * 4 in range(8)) and (
-                            board[sr][sc + i][0] == self.color[(color + 1)% 2]) and (movelog[len(movelog) - 1] ==
-                            str(self.coldic[sc + i] + self.rowdic[sr - 2 + color * 4] + self.coldic[sc + i] + self.rowdic[sr])
-                            ) and (board[sr - 1 + color * 2][sc + i] == "--") and (sr == 3 + color):
-                            self.special_moves[1].append((sr - 1 + color * 2,sc + i))
-                            self.allowed_moves.append((sr - 1 + color * 2,sc + i))
-                        if sr == 1 + color * 5:
-                            self.special_moves[2].append((sr - 1 + color * 2,sc + i))
-
+            self.pawn_move(board,movelog,sr,sc,color)
         if board[sr][sc][1] == "R":
-            for color in range(2):
-                if board[sr][sc][0] == self.color[color]:
-                    for rc in [[0,1],[0,-1],[1,0],[-1,0]]:
-                        for i in range(1,8):
-                            r = sr + rc[0] * i
-                            c = sc + rc[1] * i
-                            if r in range(8) and c in range(8):
-                                if board[r][c] == "--":
-                                    self.allowed_moves.append((r,c))
-                                if board[r][c][0] == self.color[color]:
-                                    break
-                                if board[r][c][0] == self.color[(color+1)%2]:
-                                    self.allowed_moves.append((r,c))
-                                    break
-
+            self.rook_move(board,sr,sc,color)
         if board[sr][sc][1] == "B":
-            for color in range(2):
-                if board[sr][sc][0] == self.color[color]:
-                    for rc in [[1,1],[-1,1],[1,-1],[-1,-1]]:
-                        for i in range(1,8):
-                            r = sr + rc[0] * i
-                            c = sc + rc[1] * i
-                            if r in range(8) and c in range(8):
-                                if board[r][c] == "--":
-                                    self.allowed_moves.append((r,c))
-                                if board[r][c][0] == self.color[color]:
-                                    break  
-                                if board[r][c][0] == self.color[(color+1)%2]:
-                                    self.allowed_moves.append((r,c))
-                                    break
+            self.bishop_move(board,sr,sc,color)
         if board[sr][sc][1] == "N":
-            for color in range(2):
-                if board[sr][sc][0] == self.color[color]:
-                    for rc in [[1,2],[2,1],[-1,2],[-2,1],[1,-2],[2,-1],[-1,-2],[-2,-1]]:
-                        r = sr + rc[0]
-                        c = sc + rc[1]
-                        if r in range(8) and c in range(8):
-                            if board[r][c] == "--":
-                                self.allowed_moves.append((r,c))
-                            if board[r][c][0] == self.color[(color+1)%2]:
-                                self.allowed_moves.append((r,c))
+            self.knight_move(board,sr,sc,color)
         if board[sr][sc][1] == "Q":
-            for color in range(2):
-                if board[sr][sc][0] == self.color[color]:
-                    for rc in [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,1],[1,-1],[-1,-1]]:
-                        for i in range(1,8):
-                            r = sr + rc[0] * i
-                            c = sc + rc[1] * i
-                            if r in range(8) and c in range(8):
-                                if board[r][c] == "--":
-                                    self.allowed_moves.append((r,c))
-                                if board[r][c][0] == self.color[color]:
-                                    break
-                                if board[r][c][0] == self.color[(color+1)%2]:
-                                    self.allowed_moves.append((r,c))
-                                    break
+            self.queen_move(board,sr,sc,color)
         if board[sr][sc][1] == "K":
-            for color in range(2):
-                if board[sr][sc][0] == self.color[color]:
-                    for r in [-1,0,1]:
-                        for c in [-1,0,1]:
-                            if sr + r in range(8) and sc + c in range(8) and not r == c == 0:
-                                if board[sr + r][sc + c] == "--":
-                                    self.allowed_moves.append((sr + r, sc + c))
-                                if board[sr + r][sc + c][0] == self.color[(color+1)%2]:
-                                    self.allowed_moves.append((sr + r, sc + c))
-                    for ks in movelog:
-                        if ks[0:2] == ("e" + str(1+7*color)):
-                            self.castling[color] = [False,False]
-                        if ks[0:2] == ("h" + str(1+7*color)):
-                            self.castling[color][0] = [False]
-                        if ks[0:2] == ("a" + str(1+7*color)):
-                            self.castling[color][1] = [False]
-                    r = 7 - color * 7
-                    if (self.castling[color][0]) and (not self.isthreated(board,r,4)) and (
-                        not self.isthreated(board,r,5)) and (not self.isthreated(board,r,6)
-                        ) and (board[r][5] == board[r][6] == "--"):
-                        self.special_moves[0].append((r,6))
-                        self.allowed_moves.append((r,6))
-                    if (self.castling[color][0]) and (not self.isthreated(board,r,4)) and (
-                        not self.isthreated(board,r,3)) and (not self.isthreated(board,r,2)
-                        )and (not self.isthreated(board,r,1)) and (
-                        board[r][3] == board[r][2] == board[r][1] == "--"):
-                        self.special_moves[0].append((r,2))
-                        self.allowed_moves.append((r,2))
+            self.king_move(board,movelog,sr,sc,color)
+
+        # for i in self.allowed_moves:
+        #     if not self.test_check(board,sr,sc,i[0],i[1]):
+        #         self.allowed_moves.remove(i)
+        #         print(self.allowed_moves)
+
+    def pawn_move(self,board,movelog,sr,sc,color):
+        if sr == 6 - color * 5 and board[5 - color * 3][sc] == "--" and board[4 - color][sc] == "--":
+            self.allowed_moves.append((5 - color * 3,sc))
+            self.allowed_moves.append((4 - color,sc))
+        elif sr - 1 + color * 2 in range(8) and board[sr - 1 + color * 2][sc] == "--":
+            self.allowed_moves.append((sr - 1 + color * 2,sc))
+            if sr == 1 + color * 5:
+                self.special_moves[2].append((sr - 1 + color * 2,sc))
+        for i in [-1,1]:
+            if sc + i in range(8) and (board[sr - 1 + color * 2][sc + i][0] == self.color[(color + 1) % 2]):
+                self.allowed_moves.append((sr - 1 + color * 2,sc + i))
+            if sc + i in range(8) and (sr - 2 + color * 4 in range(8)) and (
+                board[sr][sc + i][0] == self.color[(color + 1)% 2]) and (movelog[len(movelog) - 1] ==
+                str(self.coldic[sc + i] + self.rowdic[sr - 2 + color * 4] + self.coldic[sc + i] + self.rowdic[sr])
+                ) and (board[sr - 1 + color * 2][sc + i] == "--") and (sr == 3 + color):
+                self.special_moves[1].append((sr - 1 + color * 2,sc + i))
+                self.allowed_moves.append((sr - 1 + color * 2,sc + i))
+            if sr == 1 + color * 5:
+                self.special_moves[2].append((sr - 1 + color * 2,sc + i))
+    
+    def rook_move(self,board,sr,sc,color):
+        for rc in [[0,1],[0,-1],[1,0],[-1,0]]:
+            for i in range(1,8):
+                r = sr + rc[0] * i
+                c = sc + rc[1] * i
+                if r in range(8) and c in range(8):
+                    if board[r][c] == "--":
+                        self.allowed_moves.append((r,c))
+                    if board[r][c][0] == self.color[color]:
+                        break
+                    if board[r][c][0] == self.color[(color + 1) % 2]:
+                        self.allowed_moves.append((r,c))
+                        break
+
+    def bishop_move(self,board,sr,sc,color):
+        for rc in [[1,1],[-1,1],[1,-1],[-1,-1]]:
+            for i in range(1,8):
+                r = sr + rc[0] * i
+                c = sc + rc[1] * i
+                if r in range(8) and c in range(8):
+                    if board[r][c] == "--":
+                        self.allowed_moves.append((r,c))
+                    if board[r][c][0] == self.color[color]:
+                        break  
+                    if board[r][c][0] == self.color[(color+1)%2]:
+                        self.allowed_moves.append((r,c))
+                        break
+
+    def knight_move(self,board,sr,sc,color):
+        for rc in [[1,2],[2,1],[-1,2],[-2,1],[1,-2],[2,-1],[-1,-2],[-2,-1]]:
+            r = sr + rc[0]
+            c = sc + rc[1]
+            if (r in range(8) and c in range(8)) and (board[r][c] == "--" or 
+            board[r][c][0] == self.color[(color+1)%2]):
+                self.allowed_moves.append((r,c))
+    
+    def queen_move(self,board,sr,sc,color):
+        for rc in [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,1],[1,-1],[-1,-1]]:
+            for i in range(1,8):
+                r = sr + rc[0] * i
+                c = sc + rc[1] * i
+                if r in range(8) and c in range(8):
+                    if board[r][c] == "--":
+                        self.allowed_moves.append((r,c))
+                    if board[r][c][0] == self.color[color]:
+                        break
+                    if board[r][c][0] == self.color[(color+1)%2]:
+                        self.allowed_moves.append((r,c))
+                        break
+    
+    def king_move(self,board,movelog,sr,sc,color):
+        for r in [-1,0,1]:
+            for c in [-1,0,1]:
+                if sr + r in range(8) and sc + c in range(8) and not r == c == 0:
+                    if board[sr + r][sc + c] == "--" or board[sr + r][sc + c][0] == self.color[(color+1)%2]:
+                        self.allowed_moves.append((sr + r, sc + c))
+        for ks in movelog:
+            if ks[0:2] == ("e" + str(1+7*color)):
+                self.castling[color] = [False,False]
+
+            if ks[0:2] == ("h" + str(1+7*color)):
+                self.castling[color][0] = [False]
+
+            if ks[0:2] == ("a" + str(1+7*color)):
+                self.castling[color][1] = [False]
+        r = 7 - color * 7
+        if (self.castling[color][0],self.isthreated(board,r,4),self.isthreated(board,r,5),self.isthreated(board,r,6)
+            ) == (True,False,False,False) and (board[r][5] == board[r][6] == "--"):
+            self.special_moves[0].append((r,6))
+            self.allowed_moves.append((r,6))
+        if (self.castling[color][0],self.isthreated(board,r,4),self.isthreated(board,r,3),
+            self.isthreated(board,r,2),self.isthreated(board,r,1)) == (True,False,False,False,False
+            ) and (board[r][3] == board[r][2] == board[r][1] == "--"):
+            self.special_moves[0].append((r,2))
+            self.allowed_moves.append((r,2))
+        
+    def find_threated_square(self,board):
+        a = self.allowed_moves
+        for r in range(8):
+            for c in range(8):
+                self.allowed_moves = []
+                if board[r][c][0] == "w":
+                    color = 0
+                elif board[r][c][0] == "b":
+                    color = 1
+                if board[r][c][1] == "p":
+                    for i in [-1,1]:
+                        if c + i in range(8) and (board[r - 1 + color * 2][c + i][0] == self.color[(color + 1) % 2] or 
+                        board[r - 1 + color * 2][c + i] == "--"):
+                            if (r - 1 + color * 2,c + i) not in self.threated_squares[color]:
+                                self.threated_squares[color].append((r - 1 + color * 2,c + i))
+                if board[r][c][1] == "R":
+                    self.rook_move(board,r,c,color)
+                    for i in self.allowed_moves:
+                        if i not in self.threated_squares[color]:
+                            self.threated_squares[color].append(i)
+                if board[r][c][1] == "B":
+                    self.bishop_move(board,r,c,color)
+                    for i in self.allowed_moves:
+                        if i not in self.threated_squares[color]:
+                            self.threated_squares[color].append(i)
+                if board[r][c][1] == "N":
+                    self.knight_move(board,r,c,color)
+                    for i in self.allowed_moves:
+                        if i not in self.threated_squares[color]:
+                            self.threated_squares[color].append(i)
+                if board[r][c][1] == "Q":
+                    self.queen_move(board,r,c,color)
+                    for i in self.allowed_moves:
+                        if i not in self.threated_squares[color]:
+                            self.threated_squares[color].append(i)
+                if board[r][c][1] == "K":
+                    for i in [-1,0,1]:
+                        for j in [-1,0,1]:
+                            if r + i in range(8) and c + j in range(8) and not i == j == 0:
+                                if board[r + i][c + j] == "--" or board[r + i][c + j][0] == self.color[(color+1)%2]:
+                                    if (r + i, c + j) not in self.threated_squares[color]:
+                                        self.threated_squares[color].append((r + i, c + j))
+        self.allowed_moves = a
 
     def isthreated(self,board,r,c):
+        self.find_threated_square(board)
+        if board[r][c][0] == "w":
+            color = 0
+        elif board[r][c][0] == "b":
+            color = 1
+        else:
+            if r == 0:
+                color = 1
+            if r == 7:
+                color = 0
+        if (r,c) in self.threated_squares[(color + 1) % 2]:
+            return True
         return False
 
-    def check():
-        pass
+    def ischecked(self,board,color):
+        if color == 0:
+            if self.isthreated(board,self.wk[0],self.wk[1]):
+                return True
+            else:
+                return False
+        else:
+            if self.isthreated(board,self.bk[0],self.bk[1]):
+                return True
+            else:
+                return False
+    
+    def test_check(self,board,sr,sc,er,ec):
+        if board[sr][sc][0] == "w":
+            color = 0
+        elif board[sr][sc][0] == "b":
+            color = 1
+        else:
+            return 1
+        testboard = board
+        for i in testboard:
+            print(i)
+        testboard[er][ec] = testboard[sr][sc]
+        testboard[sr][sc] = "--"
+        if self.ischecked(testboard,color):
+            return False
+        else:
+            return True
 
     def move_piece(self,board,sr,sc,er,ec):
         if (er,ec) in self.special_moves[0]:
             board[er][ec] = board[sr][sc]
             board[sr][sc] = "--"
+            if board[er][ec] == "wK":
+                self.wk = (er,ec)
+            else:
+                self.bk = (er,ec)
             if ec == 6:
                 board[er][5] = board[er][7]
                 board[er][7] = "--"
@@ -163,9 +261,19 @@ class move:
                 board[4][ec] = "--"
         elif (er,ec) in self.special_moves[2]:
             print("promotion")
+            board[er][ec] = board[sr][sc][0] + "Q"
+            board[sr][sc] = "--"
+            # self.display_promotion()
         else:
+            if board[sr][sc] == "wK":
+                self.wk = (er,ec)
+            if board[sr][sc] == "bK":
+                self.bk = (er,ec)
             board[er][ec] = board[sr][sc]
             board[sr][sc] = "--"
+
+    def display_promotion():
+        pass
 
     def display_allowed_move(self,screen,board):
         for i in self.allowed_moves:
